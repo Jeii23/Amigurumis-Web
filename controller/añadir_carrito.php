732e3ce0c1d428974b89e0424523074ba332681c
Jeii23 <1636129@uab.cat>
2024-01-17 21:27:46 +0100

    Acabat pero falta 1

--- controller/añadir_carrito.php
+++ controller/añadir_carrito.php
@@ -4,17 +4,15 @@ session_start();
 $product_id = $_POST['id'];
 $quantity = $_POST['quantity'];
 
-// Comprueba si ya hay un carrito en la sesión. Si no, crea uno.
+
 if (!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = [];
 }
 
-// Comprueba si el producto ya está en el carrito. Si no, lo añade.
 if (!isset($_SESSION['cart'][$product_id])) {
     $_SESSION['cart'][$product_id] = 0;
 }
 
-// Añade la cantidad especificada del producto al carrito.
 $_SESSION['cart'][$product_id] += $quantity;
 
 echo "Producto añadido al carrito.";
--- model/categories.php
+++ model/categories.php
@@ -5,13 +5,13 @@ function getProductes($password_a8) {
     // Conecta a la base de datos
     $conn = connectaDB($password_a8);
 
-    // Prepara la consulta SQL para obtener las categorías
+    // SQL
     $sql = "SELECT * FROM public.products;";
 
-    // Ejecuta la consulta y obtén los resultados
+    // Consulta y resultats
     $result = pg_query($conn, $sql);
 
-    // Verifica si la consulta fue exitosa
+   
     if ($result) {
         // Si hay resultados, los guarda en un array
         $products = [];
@@ -28,13 +28,13 @@ function getProductById($id, $password_a8) {
     // Conecta a la base de datos
     $conn = connectaDB($password_a8);
 
-    // Prepara la consulta SQL para obtener las categorías
+    // SQL
     $sql = "SELECT * FROM public.products WHERE id = $1;";
     $params = [$id];
-    // Ejecuta la consulta y obtén los resultados
+    // Consulta y resultats
     $result = pg_query_params($conn, $sql,$params);
 
-    // Verifica si la consulta fue exitosa
+ 
     if ($result) {
         // Si hay resultados, los guarda en un array
         $products = [];
@@ -53,13 +53,12 @@ function getProductByCategory($category, $password_a8) {
     // Conecta a la base de datos
     $conn = connectaDB($password_a8);
 
-    // Prepara la consulta SQL para obtener las categorías
+    // SQL
     $sql = "SELECT * FROM public.products WHERE categoria_id = $1;";
     $params = [$category];
-    // Ejecuta la consulta y obtén los resultados
+    // Consulta y resultats
     $result = pg_query_params($conn, $sql,$params);
 
-    // Verifica si la consulta fue exitosa
     if ($result) {
         // Si hay resultados, los guarda en un array
         $products = [];
@@ -77,23 +76,13 @@ function getCategories($password_a8) {
     // Conecta a la base de datos
     $conn = connectaDB($password_a8);
 
-    // Prepara la consulta SQL para obtener las categorías
+    // SQL
     $sql = "SELECT * FROM public.categoria;";
     
 
     // Prepara la declaración
     $result = pg_query($conn, $sql);
 
-
-    // Si se proporcionó una categoría, vincula el parámetro
-    
-
-    // Ejecuta la consulta
-
-    // Obtiene los resultados
-    
-
-    // Verifica si la consulta fue exitosa
     if ($result) {
         // Si hay resultados, los guarda en un array
         $products = [];
@@ -111,14 +100,13 @@ function searchProducts($searchTerm, $password_a8) {
     // Conecta a la base de datos
     $conn = connectaDB($password_a8);
 
-    // Prepara la consulta SQL para buscar productos
+    // SQL
     $sql = "SELECT * FROM public.products WHERE nom LIKE $1 OR descripcio LIKE $1;";
     $params = ['%' . $searchTerm . '%'];
 
-    // Ejecuta la consulta y obtén los resultados
+    // Consulta y resultats
     $result = pg_query_params($conn, $sql, $params);
 
-    // Verifica si la consulta fue exitosa
     if ($result) {
         // Si hay resultados, los guarda en un array
         $products = [];
